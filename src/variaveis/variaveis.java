e79b5500b17fea9c3fcaa4414282b00d7bedee29
Danilo jesus <danilocuencas@hotmail.com>
2022-05-27 11:42:28 -0300

    Primeiro arquivo

---
+++ src/variaveis/variaveis.java
@@ -0,0 +1,21 @@
+package variaveis;
+
+public class variaveis {
+    public static void main(String[] args) {
+         int M;
+         int I;
+         int L;
+         int P;
+         int H;
+
+         M = 5;
+         I = 10;
+         L = 20;
+
+         final int j = 10;
+
+
+
+
+    }
+}
